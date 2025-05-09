62050d04f321dd56b50eae53c1b33a21c1a32e0a
Bay Pablo <bay.pablo.santiago@gmail.com>
2025-05-09 09:03:41 -0300

    table with correct columns, without pagination or sort

--- src/app/form-table/form-table.component.ts
+++ src/app/form-table/form-table.component.ts
@@ -1,27 +1,31 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatTableModule, MatTable } from '@angular/material/table';
-import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
-import { MatSortModule, MatSort } from '@angular/material/sort';
 import { FormTableDataSource, FormTableItem } from './form-table-datasource';
 
 @Component({
   selector: 'app-form-table',
   templateUrl: './form-table.component.html',
   styleUrl: './form-table.component.scss',
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule]
+  imports: [MatTableModule]
 })
 export class FormTableComponent implements AfterViewInit {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+
   @ViewChild(MatTable) table!: MatTable<FormTableItem>;
   dataSource = new FormTableDataSource();
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = [
+    'D.A.', 
+    'N° de Solicitud',
+    'Ejercicio', 
+    'N° de Orden de Pago', 
+    'Concepto, Proveedor o Contratista', 
+    'Vencimiento y/o Periodo', 
+    'Importe Solicitado', 
+    'Fuente de Financiamiento', 
+    'Cuenta Corriente a la cual acreditar'];
 
   ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
   }
 }
