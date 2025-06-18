1f4ee07e5fdc16cf82519b36678cfb37d89b116f
Bay Pablo <bay.pablo.santiago@gmail.com>
2025-06-18 09:35:08 -0300

    authenticated guard to provent login from loged user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { FormTableComponent } from './components/form-table/form-table.component';
 import { LoginComponent } from './components/login/login.component';
 import { NewsComponent } from './components/news/news.component';
-import { authGuard } from './guards/auth.guard';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthenticatedGuard } from './guards/authenticated.guard';
 
 export const routes: Routes = [
 
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'form-table', component: FormTableComponent, canActivate: [authGuard] },
-    { path: 'news', component: NewsComponent, canActivate: [authGuard] }
-
+    { path: 'login', component: LoginComponent, canActivate: [AuthenticatedGuard] },
+    { path: 'form-table', component: FormTableComponent, canActivate: [AuthGuard] },
+    { path: 'news', component: NewsComponent, canActivate: [AuthGuard] },
+    { path: '**', redirectTo: 'login' }
 ];
--- src/app/guards/authenticated.guard.ts
+++ src/app/guards/authenticated.guard.ts
@@ -1,5 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
-export const authenticatedGuard: CanActivateFn = (route, state) => {
-  return true;
+export const AuthenticatedGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+    const router = inject(Router);
+  
+    if (authService.isAuthenticated()){
+      return router.navigate(['/news']);
+    }else{
+      return true;
+    }
 };
