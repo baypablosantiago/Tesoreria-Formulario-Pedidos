c00f170ab50faf0f5f9eed2c83087a13cf815c6d
Bay Pablo <bay.pablo.santiago@gmail.com>
2025-07-04 12:01:27 -0300

    close approach

--- src/app/components/actions-modal/actions-modal.component.ts
+++ src/app/components/actions-modal/actions-modal.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { FundingRequestService } from '../../services/funding-request.service';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-actions-modal',
@@ -17,24 +18,34 @@ export class ActionsModalComponent {
   constructor(
     public dialogRef: MatDialogRef<ActionsModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: FundingRequest,
-    private fundingRequestService: FundingRequestService
+    private fundingRequestService: FundingRequestService,
+    private router: Router
   ) { }
 
   close(): void {
-    this.dialogRef.close();
+  this.dialogRef.close();
+
+  if (this.data.isActive) {
+    this.router.navigateByUrl('/dashboard');
+  } else {
+    this.router.navigateByUrl('/finished-requests');
   }
+}
+
 
   onToggleActive(): void {
-    if (!this.data?.id) return;
+  if (!this.data?.id) return;
 
-    this.fundingRequestService.changeIsActive(this.data.id).subscribe({
-      next: updatedRequest => {
-        this.dialogRef.close({ updatedRequest }); 
-      },
-      error: err => {
-        console.error('Error al cambiar estado activo', err);
-      }
-    });
-  }
+  this.fundingRequestService.changeIsActive(this.data.id).subscribe({
+    next: updatedRequest => {
+      this.data.isActive = updatedRequest.isActive; 
+    },
+    error: err => {
+      console.error('Error al cambiar estado activo', err);
+    }
+  });
+}
+
 
 }
+
